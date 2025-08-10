74ad69cc783c26cd9a56cd3ce58b24d19d01466d
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-10 13:41:37 +0530

    initial commit

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,3 +1,7 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {};
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONDODB_URI);
+  } catch (error) {}
+};
