f43b8f210205a8b0c9b684068197ae07b7e178be
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-12 22:53:09 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,25 +2,7 @@ import { useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="hero bg-black  min-h-screen">
-      <div className="hero-content flex-col lg:flex-row">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl border-b-amber-50 border-4"
-        />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
-        </div>
-      </div>
-    </div>
-  );
+  return;
 }
 
 export default App;
