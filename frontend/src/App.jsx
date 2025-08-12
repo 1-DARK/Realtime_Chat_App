4df0897672f42d108c0c388fcaf63d56b2a47a1b
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-12 23:09:12 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import { useEffect, useRef } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Homepage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
