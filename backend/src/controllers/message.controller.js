a4c8ac1770a608581d43c8a2b6cdb6c71efc6a3c
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-12 22:04:05 +0530

    initial commit

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -15,23 +15,19 @@ export const getUsersForSidebar = async (req, res) => {
 
 export const getMessages = async (req, res) => {
   try {
-    const { id: userToChatID } = req.params;
+    const { id: userToChatId } = req.params;
     const myId = req.user._id;
+
     const messages = await Message.find({
       $or: [
-        {
-          myId: myId,
-          receiverId: userToChatID,
-        },
-        {
-          myId: userToChatID,
-          receiverId: myId,
-        },
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
       ],
     });
+
     res.status(200).json(messages);
   } catch (error) {
-    console.log("Error in getMessages controller", error.message);
+    console.log("Error in getMessages controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
