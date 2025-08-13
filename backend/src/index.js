908d1ff18874047a47099faf908ef983285d88ee
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-13 19:40:36 +0530

    initial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,18 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.listen(PORT, () => {
