4771107355e9f6a6a2c50f96b7a388526a52218b
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-20 20:06:25 +0530

    initial commit

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,6 +1,13 @@
-import React from "react";
-
+import { useRef, useState } from "react";
+import { useChatStore } from "../store/useChatStore.js";
 const MessageInput = () => {
+  const [text, setText] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const fileInputRef = useRef(null);
+  const { sendMessage } = useChatStore();
+  const handleImageChange = (e) => {};
+  const removeImage = () => {};
+  const handleSendMessage = async (e) => {};
   return <div>Hello</div>;
 };
 
