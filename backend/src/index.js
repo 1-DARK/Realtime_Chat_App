1a398e8854fe6f1aa6025083e63c627c8a260b99
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-10-01 00:05:18 +0530

    init

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,10 @@ import { connectDB } from "./lib/db.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { app, server } from "./lib/socket.js";
-
+import path from "path";
 dotenv.config();
-
-const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -20,6 +20,15 @@ app.use(
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
   connectDB();
